ba340a63dcb7c735bd188943c8cc5c5b00f36fcb
eliancruz29 <eliancruz29@gmail.com>
2021-07-06 00:14:51 -0400

    adding the complete bottom bar

---
+++ udemy/ud_flutter_firestore_complexapp/lib/Pages/item_two.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemTwo extends StatefulWidget {
+  const ItemTwo({Key? key}) : super(key: key);
+
+  @override
+  _ItemTwoState createState() => _ItemTwoState();
+}
+
+class _ItemTwoState extends State<ItemTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- udemy/ud_flutter_firestore_complexapp/lib/home.dart
+++ udemy/ud_flutter_firestore_complexapp/lib/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ud_flutter_firestire_complexapp/Pages/item_one.dart';
+import 'package:ud_flutter_firestire_complexapp/Pages/item_two.dart';
+import 'package:ud_flutter_firestire_complexapp/Pages/item_three.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,12 +13,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indexPage = 1;
+  final pageOptions = [
+    const ItemOne(),
+    const ItemTwo(),
+    const ItemThree(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Complex App"),
         backgroundColor: Colors.deepOrange,
+      ),
+      body: pageOptions[_indexPage],
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.deepOrange,
+        buttonBackgroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: const Duration(
+          milliseconds: 600,
+        ),
+        index: _indexPage,
+        items: const <Widget>[
+          Icon(
+            Icons.poll,
+            size: 30.0,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.home,
+            size: 30.0,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.library_books,
+            size: 30.0,
+            color: Colors.white,
+          ),
+        ],
+        onTap: (int index) {
+          _indexPage = index;
+        },
       ),
     );
   }
