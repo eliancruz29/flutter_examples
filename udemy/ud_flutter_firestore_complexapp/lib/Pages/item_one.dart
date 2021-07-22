00cccd9fea992dffff38654826446463297a84e3
eliancruz29 <eliancruz29@gmail.com>
2021-07-22 00:11:39 -0400

    getting one post from firestore

--- udemy/ud_flutter_firestore_complexapp/lib/Pages/item_one.dart
+++ udemy/ud_flutter_firestore_complexapp/lib/Pages/item_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ItemOne extends StatefulWidget {
   const ItemOne({Key? key}) : super(key: key);
@@ -8,8 +9,95 @@ class ItemOne extends StatefulWidget {
 }
 
 class _ItemOneState extends State<ItemOne> {
+  Future<List<DocumentSnapshot>> getPost() async {
+    var firestore = FirebaseFirestore.instance;
+    QuerySnapshot snap = await firestore.collection("itemone").get();
+    return snap.docs;
+  }
+
+  Future getRefresh() async {
+    await Future.delayed((const Duration(seconds: 3)));
+    setState(() {
+      getPost();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: FutureBuilder(
+        future: getPost(),
+        builder: (context, AsyncSnapshot<List<DocumentSnapshot>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return RefreshIndicator(
+              onRefresh: getRefresh,
+              child: ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  var ourData = snapshot.data?[index];
+
+                  return Container(
+                    height: 170.0,
+                    margin: const EdgeInsets.all(10.0),
+                    child: Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.network(
+                                ourData?.get("img"),
+                                height: 100.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 15.0),
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  ourData?.get("title"),
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5.0),
+                                Text(
+                                  ourData?.get("des"),
+                                  maxLines: 5,
+                                  style: const TextStyle(
+                                    fontSize: 17.0,
+                                    color: Colors.black,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
