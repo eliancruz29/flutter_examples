ba85e51d679945f2770ffd5f697ce4438a9c665c
eliancruz29 <eliancruz29@gmail.com>
2021-07-22 23:19:24 -0400

    Custom button desing

--- udemy/ud_flutter_firestore_complexapp/lib/Pages/item_one.dart
+++ udemy/ud_flutter_firestore_complexapp/lib/Pages/item_one.dart
@@ -22,6 +22,16 @@ class _ItemOneState extends State<ItemOne> {
     });
   }
 
+  final List<MaterialColor> _colorItems = [
+    Colors.green,
+    Colors.purple,
+    Colors.amber,
+    Colors.yellow,
+    Colors.deepOrange,
+    Colors.pink,
+    Colors.red,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,9 +49,10 @@ class _ItemOneState extends State<ItemOne> {
                 itemCount: snapshot.data?.length,
                 itemBuilder: (context, index) {
                   var ourData = snapshot.data?[index];
+                  var color = _colorItems[index % _colorItems.length];
 
                   return Container(
-                    height: 170.0,
+                    height: 200.0,
                     margin: const EdgeInsets.all(10.0),
                     child: Card(
                       elevation: 10,
@@ -56,7 +67,7 @@ class _ItemOneState extends State<ItemOne> {
                               borderRadius: BorderRadius.circular(20.0),
                               child: Image.network(
                                 ourData?.get("img"),
-                                height: 100.0,
+                                height: 200.0,
                                 fit: BoxFit.cover,
                               ),
                             ),
@@ -84,7 +95,25 @@ class _ItemOneState extends State<ItemOne> {
                                     fontSize: 17.0,
                                     color: Colors.black,
                                   ),
-                                )
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(10.0),
+                                    margin: const EdgeInsets.all(10.0),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      color: color,
+                                    ),
+                                    child: const Text(
+                                      "View detail",
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
                               ],
                             ),
                           )
