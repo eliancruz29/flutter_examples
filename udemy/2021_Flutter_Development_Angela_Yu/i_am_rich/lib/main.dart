c284e0a4c45b80e7df59c4b6fd29a7c26e026148
eliancruz29 <eliancruz29@gmail.com>
2021-08-09 22:48:28 -0400

    fixing error with I am rich app

--- udemy/2021_Flutter_Development_Angela_Yu/i_am_rich/lib/main.dart
+++ udemy/2021_Flutter_Development_Angela_Yu/i_am_rich/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
@@ -18,6 +25,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
