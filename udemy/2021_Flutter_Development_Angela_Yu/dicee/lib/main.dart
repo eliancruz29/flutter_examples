6d2e8d5ac39e6da2b3a9801a15d641eaed7164ac
eliancruz29 <eliancruz29@gmail.com>
2021-11-24 00:20:41 -0400

    finishing the dice app example

--- udemy/2021_Flutter_Development_Angela_Yu/dicee/lib/main.dart
+++ udemy/2021_Flutter_Development_Angela_Yu/dicee/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => new _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceNumbers() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () => changeDiceNumbers(),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => changeDiceNumbers(),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
